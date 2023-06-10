6d8b3544d60ca7ff1effc096fec96be40659f74b
Maaz <maaz.d.sid@gmail.com>
2023-06-10 22:34:24 +0500

    Make task list and page container responsive

--- src/features/dashboard/Dashboard.tsx
+++ src/features/dashboard/Dashboard.tsx
@@ -5,11 +5,11 @@ import TimeTracker from './time-tracker/TimeTracker';
 const Dashboard = () => {
   return (
     <div className="page-container">
-      <Grid container spacing={10}>
-        <Grid item xs={12} lg={8}>
+      <Grid container rowSpacing={{ xs: 5 }} columnSpacing={{ xs: 10, lg: 10 }}>
+        <Grid item xs={12} lg={8} order={{ xs: 2, lg: 1 }}>
           <TaskList />
         </Grid>
-        <Grid item xs={12} lg={4}>
+        <Grid item xs={12} lg={4} order={{ xs: 1, lg: 2 }}>
           <TimeTracker />
         </Grid>
       </Grid>
