e1deb5683da61dbad7c72b551ede506b90780314
Maaz <maaz.d.sid@gmail.com>
2023-08-21 23:29:39 +0500

    integrated APIs for time tracker

--- src/features/dashboard/time-tracker/TimeTracker.tsx
+++ src/features/dashboard/time-tracker/TimeTracker.tsx
@@ -1,14 +1,46 @@
 import { Card, CardContent, IconButton, MenuItem, Menu, TextField } from '@mui/material';
 import PlayCircleIcon from '@mui/icons-material/PlayCircle';
 import StopCircleIcon from '@mui/icons-material/StopCircle';
-import { useState, MouseEvent } from 'react';
+import { useState, MouseEvent, useMemo, useEffect } from 'react';
 import EllipsisIcon from '@components/svgs/EllipsisIcon';
 import classes from './TimeTracker.module.scss';
+import { useMutation, useQuery, useQueryClient } from 'react-query';
+import { createTask, deleteTask, getTasks, updateTask } from 'api/tasks/tasks-api';
+import { Controller, useForm } from 'react-hook-form';
+import { CreateTaskReq } from 'api/tasks/models/create-task/create-task-req.model';
+import { useNotificationStore } from 'store/store';
+import { useTimeTracker } from 'hooks/useTimeTracker';
+import { UpdateTaskReq } from 'api/tasks/models/update-task/update-task-req.model';
 
 const TimeTracker = () => {
   const [optionsAnchor, setOptionsAnchor] = useState<HTMLElement | null>(null);
-  const [isPlaying, setIsPlaying] = useState(false);
-  const [isLoading, setIsLoading] = useState(false);
+  const showNotification = useNotificationStore((state) => state.showNotification);
+  const queryClient = useQueryClient();
+
+  const { data: tasks } = useQuery('tasks', () => getTasks({ page: '1', pageSize: '10' }));
+  const { isLoading: isCreateTaskLoading, mutate: createTaskReq } = useMutation(createTask);
+  const { mutate: updateTaskReq } = useMutation(updateTask);
+  const { mutate: deleteTaskReq } = useMutation(deleteTask);
+
+  const formattedTime = useTimeTracker(tasks || []);
+
+  const { control, getValues, reset } = useForm({
+    defaultValues: {
+      description: '',
+    },
+  });
+
+  useEffect(() => {
+    const task = tasks?.find((t) => {
+      return !t?.endTime;
+    });
+
+    if (task) {
+      reset({
+        description: task?.description,
+      });
+    }
+  }, [tasks, reset]);
 
   const openOptionsMenu = (event: MouseEvent<HTMLElement>) => {
     setOptionsAnchor(event.currentTarget);
@@ -16,18 +48,87 @@ const TimeTracker = () => {
 
   const closeOptionsMenu = () => {
     setOptionsAnchor(null);
+
+    if (!task) return;
+
+    deleteTaskReq(task.id?.toString(), {
+      onSuccess: () => {
+        queryClient.invalidateQueries('tasks');
+        showNotification('Task deleted successfully!', 'success');
+      },
+      onError: (error: any) => {
+        const errorMessage = error?.response?.data?.messages?.[0] || 'Something went wrong.';
+        showNotification(errorMessage);
+      },
+    });
   };
 
-  const toggleIsPlaying = () => {
-    if(!isPlaying) {
-      return;
-    }
-    setIsPlaying(!isPlaying);
+  const startTimer = () => {
+    const { description } = getValues() || {};
+    const requestBody: CreateTaskReq = { description, startTime: new Date().toISOString() };
+
+    createTaskReq(requestBody, {
+      onSuccess: () => {
+        queryClient.invalidateQueries('tasks');
+        showNotification('Task added successfully!', 'success');
+      },
+      onError: (error: any) => {
+        const errorMessage = error?.response?.data?.messages?.[0] || 'Something went wrong.';
+        showNotification(errorMessage);
+      },
+    });
   };
+
+  const stopTimer = () => {
+    if (!task) return;
+
+    const { description } = getValues() || {};
+
+    const requestBody: UpdateTaskReq = {
+      description: description,
+      startTime: task.startTime,
+      endTime: new Date().toISOString(),
+    };
+
+    updateTaskReq(
+      { reqBody: requestBody, taskId: task?.id?.toString() },
+      {
+        onSuccess: () => {
+          queryClient.invalidateQueries('tasks');
+          reset({
+            description: '',
+          });
+          showNotification('Task Updated successfully!', 'success');
+        },
+        onError: (error: any) => {
+          const errorMessage = error?.response?.data?.messages?.[0] || 'Something went wrong.';
+          showNotification(errorMessage);
+        },
+      }
+    );
+  };
+
+  const { task, isTimerRunning } = useMemo(() => {
+    const task = tasks?.find((t) => {
+      return !t?.endTime;
+    });
+
+    if (task) {
+      return {
+        task: task,
+        isTimerRunning: true,
+      };
+    }
+
+    return {
+      task: null,
+      isTimerRunning: false,
+    };
+  }, [tasks]);
 
   return (
     <>
-      <Card className={`${classes.trackerCard} ${isLoading ? classes.loading : ''}`}>
+      <Card className={`${classes.trackerCard} ${isCreateTaskLoading ? classes.loading : ''}`}>
         <CardContent>
           <header className={classes.header}>
             <h2 className={classes.headerTitle}>Time Tracker</h2>
@@ -36,31 +137,42 @@ const TimeTracker = () => {
                 <EllipsisIcon />
               </IconButton>
               <Menu open={!!optionsAnchor} anchorEl={optionsAnchor} onClose={closeOptionsMenu} elevation={4}>
-                <MenuItem className={classes.optionMenuItem} onClick={closeOptionsMenu}>
+                <MenuItem
+                  className={classes.optionMenuItem}
+                  onClick={closeOptionsMenu}
+                  disabled={!isTimerRunning}
+                >
                   Discard
                 </MenuItem>
               </Menu>
             </div>
           </header>
 
-          <TextField
-            multiline
-            maxRows={4}
-            fullWidth
-            size="small"
-            label="Enter description"
-            variant="outlined"
-          ></TextField>
+          <Controller
+            name="description"
+            control={control}
+            render={({ field }) => (
+              <TextField
+                multiline
+                maxRows={4}
+                fullWidth
+                size="small"
+                label="Enter description"
+                variant="outlined"
+                {...field}
+              />
+            )}
+          />
 
-          <h3 className={classes.timerText}>00:00:00</h3>
+          <h3 className={classes.timerText}>{formattedTime}</h3>
 
           <div className={classes.actionBtnWrapper}>
-            {isPlaying ? (
-              <IconButton onClick={toggleIsPlaying}>
+            {isTimerRunning ? (
+              <IconButton onClick={stopTimer}>
                 <StopCircleIcon className={`${classes.icon} ${classes.stop}`} />
               </IconButton>
             ) : (
-              <IconButton onClick={toggleIsPlaying}>
+              <IconButton onClick={startTimer}>
                 <PlayCircleIcon className={`${classes.icon} ${classes.play}`} />
               </IconButton>
             )}
