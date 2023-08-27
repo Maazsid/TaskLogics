f23ae5e9f8b1156c035c92600fafa85f053f1811
Maaz <maaz.d.sid@gmail.com>
2023-08-27 17:29:06 +0500

    show loading text when tab is switched

--- src/features/dashboard/Dashboard.tsx
+++ src/features/dashboard/Dashboard.tsx
@@ -1,19 +1,35 @@
 import { Grid } from '@mui/material';
 import TaskList from './task-list/TaskList';
 import TimeTracker from './time-tracker/TimeTracker';
+import { useEffect, useState } from 'react';
+import classes from './Dashboard.module.scss';
 
 const Dashboard = () => {
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    setIsLoading(false);
+  }, []);
+
   return (
-    <div className="page-container">
-      <Grid container rowSpacing={{ xs: 5 }} columnSpacing={{ xs: 10, lg: 10 }}>
-        <Grid item xs={12} lg={8} order={{ xs: 2, lg: 1 }}>
-          <TaskList />
-        </Grid>
-        <Grid item xs={12} lg={4} order={{ xs: 1, lg: 2 }}>
-          <TimeTracker />
-        </Grid>
-      </Grid>
-    </div>
+    <>
+      {isLoading ? (
+        <div className="page-container-loading">
+          <p className={classes.loadingText}>Loading, please wait...</p>
+        </div>
+      ) : (
+        <div className="page-container">
+          <Grid container rowSpacing={{ xs: 5 }} columnSpacing={{ xs: 10, lg: 10 }}>
+            <Grid item xs={12} lg={8} order={{ xs: 2, lg: 1 }}>
+              <TaskList />
+            </Grid>
+            <Grid item xs={12} lg={4} order={{ xs: 1, lg: 2 }}>
+              <TimeTracker />
+            </Grid>
+          </Grid>
+        </div>
+      )}
+    </>
   );
 };
 
